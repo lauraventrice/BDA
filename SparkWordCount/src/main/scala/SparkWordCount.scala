bfbfbc06558141082ca979afe360869b2f895c8a
LauraVentrice <laura.ventrice@edu.unito.it>
2022-04-02 19:04:45 +0200

    First try word count scala

--- SparkWordCount/src/main/scala/SparkWordCount.scala
+++ SparkWordCount/src/main/scala/SparkWordCount.scala
@@ -1,20 +1,30 @@
+import org.apache.hadoop.shaded.com.google.common.base.Predicates.not
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.SparkContext._
+
 
 object SparkWordCount {
-  def main(args: Array[String]) {
-    if (args.length < 2) {
-      System.err.println("Correct arguments: <input-directory> <output-directory>")
-      System.exit(1)
-    }
+    def main(args: Array[String]) {
+        if (args.length < 2) {
+            System.err.println("Correct arguments: <input-directory> <output-directory>")
+            System.exit(1)
+        }
 
-    val sparkConf = new SparkConf().setAppName("SparkWordCount")
-    val ctx = new SparkContext(sparkConf)
-    val textFile = ctx.textFile(args(0))
-    val counts = textFile.flatMap(line => line.split(" "))
-                 .map(word => (word, 1))
-                 .reduceByKey(_ + _)
-    counts.saveAsTextFile(args(1)) 
-    ctx.stop()
-  }
+        val sparkConf = new SparkConf().setAppName("SparkWordCount")
+        val ctx = new SparkContext(sparkConf)
+        val textFile = ctx.textFile(args(0))
+        val regexW = "^([a-z]|[\\_]|[\\-]+)(([\\-])|([\\_])|([a-z]))*$"
+        val regexN = "\\d+(\\.(\\d+))?"
+        val text = textFile.flatMap(line => line.split(" "))
+        val countsW = text.filter(s => s.matches(regexW))
+                      .map(word => (word, 1))
+                      .reduceByKey(_ + _)
+
+        val countsN = text.filter(s => s.matches(regexN))
+                    .map(word => (word, 1))
+                    .reduceByKey(_ + _)
+
+        countsW.saveAsTextFile(args(1))
+        countsN.saveAsTextFile(args(1))
+        ctx.stop()
+    }
 }
