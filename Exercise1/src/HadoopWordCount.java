e15e52a62ccadb46cd164ef1e7a074eea273febd
Gepiro <gepiro0916@gmail.com>
2022-04-02 18:30:49 +0200

    Finished (a)

--- Exercise1/src/HadoopWordCount.java
+++ Exercise1/src/HadoopWordCount.java
@@ -3,9 +3,7 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
@@ -68,6 +66,28 @@ public class HadoopWordCount extends Configured implements Tool {
         }
     }
 
+    public static class ReverseComparator extends WritableComparator {
+
+        private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
+        public ReverseComparator() {
+            super(Text.class);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+            return (-1)* TEXT_COMPARATOR.compare(b1, s1, l1, b2, s2, l2);
+        }
+
+        @SuppressWarnings("rawtypes")
+        @Override
+        public int compare(WritableComparable a, WritableComparable b) {
+            if (a instanceof Text && b instanceof Text) {
+                return (-1)*(((Text) a).compareTo((Text) b));
+            }
+            return super.compare(a, b);
+        }
+    }
+
     public static class PartitionerClass extends Partitioner<Text, IntWritable> {
         // Class for partitioning the output
         @Override
@@ -99,6 +119,8 @@ public class HadoopWordCount extends Configured implements Tool {
         job.setCombinerClass(Reduce.class);
         job.setReducerClass(Reduce.class);
         job.setPartitionerClass(PartitionerClass.class);
+
+        job.setSortComparatorClass(ReverseComparator.class);
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
--- Exercise1/src/HadoopWordStripes.java
+++ Exercise1/src/HadoopWordStripes.java
@@ -1,10 +1,7 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.MapWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
@@ -140,6 +137,28 @@ public class HadoopWordStripes extends Configured implements Tool {
 		}
 	}
 
+	public static class ReverseComparator extends WritableComparator {
+
+		private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
+		public ReverseComparator() {
+			super(Text.class);
+		}
+
+		@Override
+		public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+			return (-1)* TEXT_COMPARATOR.compare(b1, s1, l1, b2, s2, l2);
+		}
+
+		@SuppressWarnings("rawtypes")
+		@Override
+		public int compare(WritableComparable a, WritableComparable b) {
+			if (a instanceof Text && b instanceof Text) {
+				return (-1)*(((Text) a).compareTo((Text) b));
+			}
+			return super.compare(a, b);
+		}
+	}
+
 	@Override
 	public int run(String[] args) throws Exception {
 		Job job = Job.getInstance(new Configuration(), "HadoopWordStripes");
@@ -152,6 +171,8 @@ public class HadoopWordStripes extends Configured implements Tool {
 		job.setCombinerClass(Reduce.class);
 		job.setReducerClass(Reduce.class);
 		job.setPartitionerClass(PartitionerClass.class);
+
+		job.setSortComparatorClass(ReverseComparator.class);
 
 		job.setNumReduceTasks(2); // use two different reducers for numbers or words
 
