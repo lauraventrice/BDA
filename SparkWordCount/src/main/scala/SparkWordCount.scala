5d6e7dba77e34a84622cb80ab56efbdbd736c312
LauraVentrice <laura.ventrice@edu.unito.it>
2022-04-03 17:36:18 +0200

    Update 3(b) stop words

--- SparkWordCount/src/main/scala/SparkWordCount.scala
+++ SparkWordCount/src/main/scala/SparkWordCount.scala
@@ -1,8 +1,16 @@
-import org.apache.hadoop.shaded.com.google.common.base.Predicates.not
 import org.apache.spark.{SparkConf, SparkContext}
 
+import scala.io.{BufferedSource, Source}
 
 object SparkWordCount {
+
+    var filename = "../../stopwords.txt"
+    var res: BufferedSource = Source.fromFile(filename)
+    val lines: Seq[String] = res.getLines.toList
+
+
+    var neg: Boolean => Boolean = !_
+
     def main(args: Array[String]) {
         if (args.length < 2) {
             System.err.println("Correct arguments: <input-directory> <output-directory>")
@@ -17,20 +25,22 @@ object SparkWordCount {
 
         val text = textFile.flatMap(line => line.split(" "))
 
-        val countsW = text.map(s => s.toLowerCase()).filter(s => s.matches(regexW))
+        val countsW = text.map(s => s.toLowerCase()).filter(s => s.matches(regexW) && neg(lines.contains(s)))
                       .map(word => (word, 1))
                       .reduceByKey(_ + _).sortBy(_._2, ascending = false)
 
         val countsN = text.filter(s => s.matches(regexN))
                     .map(word => (word, 1))
                     .reduceByKey(_ + _).sortBy(_._2, ascending = false)
-
+        res.close()
         val topWords = countsW.take(1000)
         val topNumbers = countsN.take(1000)
         ctx.parallelize(topWords).saveAsTextFile(args(1) + "/topWords")
         ctx.parallelize(topNumbers).saveAsTextFile(args(1) + "/topNumbers")
-        countsW.saveAsTextFile(args(1) + "/wordsCount") //maybe is better saveAsObject? 
+        countsW.saveAsTextFile(args(1) + "/wordsCount") //maybe is better saveAsObject?
         countsN.saveAsTextFile(args(1) + "/numbersCount")
         ctx.stop()
+
+
     }
 }
