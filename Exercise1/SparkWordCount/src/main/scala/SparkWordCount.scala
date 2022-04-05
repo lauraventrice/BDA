9818862705e081dd2580534fc4ea11e9471a9335
LauraVentrice <laura.ventrice@edu.unito.it>
2022-04-05 11:50:50 +0200

    Feat val and broadcast variables word count

--- Exercise1/SparkWordCount/src/main/scala/SparkWordCount.scala
+++ Exercise1/SparkWordCount/src/main/scala/SparkWordCount.scala
@@ -1,13 +1,6 @@
 import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.io.{BufferedSource, Source}
-
 object SparkWordCount {
-
-    var filename = "../../stopwords.txt"
-    var res: BufferedSource = Source.fromFile(filename)
-    val lines: Seq[String] = res.getLines.toList
-    res.close()
 
     var neg: Boolean => Boolean = !_
 
@@ -16,23 +9,24 @@ object SparkWordCount {
             System.err.println("Correct arguments: <input-directory> <output-directory>")
             System.exit(1)
         }
-
+        val filename = "../Data/stopwords.txt"
         val sparkConf = new SparkConf().setAppName("SparkWordCount")
         val ctx = new SparkContext(sparkConf)
         val textFile = ctx.textFile(args(0))
+        val stopwords = ctx.textFile(filename).collect()
+
         val regexW = "^([a-z]|[\\_]|[\\-]+)(([\\-])|([\\_])|([a-z]))*$"
         val regexN = "\\d+(\\.(\\d+))?"
 
         val text = textFile.flatMap(line => line.split(" "))
-        /*
-        val broadcastVar = sc.broadcast(Array(0, 1, 2, 3))
 
-        broadcastVar.value
-         */
+        val bStopwords = ctx.broadcast(stopwords)
 
-        val counts = text.map(s => s.toLowerCase()).filter(s => (s.matches(regexW) || s.matches(regexN)) && neg(lines.contains(s)))
-                      .map(word => (word, 1))
-                      .reduceByKey(_ + _).sortBy(_._2, ascending = false)
+
+        val counts = text.map(s => s.toLowerCase())
+                    .filter(s => (s.matches(regexW) || s.matches(regexN)) && neg(stopwords.contains(s))) //or bStopwords.value
+                    .map(word => (word, 1))
+                    .reduceByKey(_ + _).sortBy(_._2, ascending = false).collect()
 
         val countsW = counts.filter(s => s._1.matches(regexW))
         val countsN = counts.filter(s => s._1.matches(regexN))
@@ -41,8 +35,8 @@ object SparkWordCount {
         val topNumbers = countsN.take(1000)
         ctx.parallelize(topWords).saveAsTextFile(args(1) + "/topWords")
         ctx.parallelize(topNumbers).saveAsTextFile(args(1) + "/topNumbers")
-        countsW.saveAsTextFile(args(1) + "/wordsCount") //maybe is better saveAsObject?
-        countsN.saveAsTextFile(args(1) + "/numbersCount")
+        ctx.parallelize(countsW).saveAsTextFile(args(1) + "/wordsCount") //persist()?
+        ctx.parallelize(countsN).saveAsTextFile(args(1) + "/numbersCount")
         ctx.stop()
 
 
