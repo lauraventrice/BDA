ec7304870911810e05b6f2811a2e546fb876add3
Gepiro <gepiro0916@gmail.com>
2022-04-17 18:32:17 +0200

    Finished (b)(i)

--- Exercise2/SparkTwitterCollector/src/main/scala/SparkTwitterCollector.scala
+++ Exercise2/SparkTwitterCollector/src/main/scala/SparkTwitterCollector.scala
@@ -1,5 +1,5 @@
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.{HashingTF, IndexToString, StringIndexer, Tokenizer}
+import org.apache.spark.ml.feature.{HashingTF, IndexToString, OneHotEncoder, StandardScaler, StringIndexer, Tokenizer, VectorAssembler}
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.sql.Row
 import org.apache.spark.ml.linalg.Vector
@@ -20,46 +20,69 @@ object SparkTwitterCollector {
       .csv(csvPath)
 
     //(b)
+    //(i)
     val Array(trainData, testData) = dataFrame.randomSplit(Array(0.9, 0.1)) //Split DataFrame in trainData and testData
 
     trainData.cache() // subset of dataset used for training
     testData.cache() // subset of dataset used for final evaluation ("testing")
 
-    //FIN QUI CREDO ABBIA SENSO; DOPO è tutto oscuro
+    //Dataset's feature
+    val index_columns = Array("Smoking", "AlcoholDrinking", "Stroke", "DiffWalking", "Sex", "Race", "Diabetic", "PhysicalActivity", "GenHealth", "Asthma", "KidneyDisease", "SkinCancer", "AgeCategory")
+    val numerical_columns = Array("BMI", "PhysicalHealth", "MentalHealth", "SleepTime")
 
+    var index_columns_OHE: Array[String] = Array()
+    var index_columns_index: Array[String] = Array()
+
+    //Create two array for input and output
+    index_columns.foreach(elem =>
+      {index_columns_OHE = index_columns_OHE.union(Array(elem + "OHE"))
+      index_columns_index = index_columns_index.union(Array(elem + "_index"))}
+    )
+
+    //Array of features' name
+    val features = index_columns_OHE ++ numerical_columns
+
+    //Transform HeartDisease from a String in an Integer named label
     val labelIndexer = new StringIndexer()
       .setInputCol("HeartDisease")
       .setOutputCol("label")
-      .fit(trainData)
       .setHandleInvalid("skip")
+      .fit(trainData)
 
-    val categoryConverter = new IndexToString()
-      .setInputCol("prediction")
-      .setOutputCol("category")
-      .setLabels(labelIndexer.labels)
+    //Transform the whole set of String features in OneHotEncoder features
+    val stringIndexer = new StringIndexer()
+      .setInputCols(index_columns)
+      .setOutputCols(index_columns_index)
+    val oneHotEncoder = new OneHotEncoder()
+      .setInputCols(index_columns_index)
+      .setOutputCols(index_columns_OHE)
 
-    val tokenizer = new Tokenizer()
-      .setInputCol("Smoking")
-      .setOutputCol("words")
-    val hashingTF = new HashingTF()
-      .setNumFeatures(1000)
-      .setInputCol(tokenizer.getOutputCol)
+    //Features' union
+    val vector = new VectorAssembler()
+      .setInputCols(features)
       .setOutputCol("features")
+
+    //Feature scaling helps in decreasing the convergence time
+    val scaler = new StandardScaler().setInputCol("features").setOutputCol("scaledFeatures")
+
+    //Regression
     val lr = new LogisticRegression()
       .setMaxIter(10)
       .setRegParam(0.001)
-    val pipeline = new Pipeline()
-      .setStages(Array(tokenizer, hashingTF, labelIndexer, lr, categoryConverter))
+      .setLabelCol("label")
+      .setFeaturesCol("scaledFeatures")
 
-    // Fit the pipeline to training documents.
+    //Pipeline
+    val stages = Array(labelIndexer, stringIndexer, oneHotEncoder, vector, scaler, lr)
+    val pipeline = new Pipeline().setStages(stages)
+
     val model = pipeline.fit(trainData)
 
-    // Make predictions on test documents.
     model.transform(testData)
-      .select("Smoking", "probability", "prediction")
+      .select("HeartDisease","probability", "prediction")
       .collect()
-      .foreach { case Row(smoking: String, prob: Vector, prediction: Double) =>
-        println(s"($smoking) --> prob=$prob, prediction=$prediction")
+      .foreach { case Row(id: String, prob: Vector, prediction: Double) =>
+        println(s"($id) --> prob=$prob, prediction=$prediction")
       }
   }
 }
