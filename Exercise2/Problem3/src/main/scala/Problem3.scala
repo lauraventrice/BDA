70e80cd937465ff3c167ddcf6a36533a41c0768a
Gepiro <gepiro0916@gmail.com>
2022-04-19 18:27:50 +0200

    Progress (iii)

--- Exercise2/Problem3/src/main/scala/Problem3.scala
+++ Exercise2/Problem3/src/main/scala/Problem3.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd._
 import org.apache.spark.mllib.recommendation._
@@ -48,5 +49,63 @@ object Problem3 {
     //TODO va bene questo metodo per training ? Il prof usa questo nel suo file
     val model = ALS.trainImplicit(trainData90, 10, 5, 0.01, 1.0)
 
+    //(iii)
+
+    //TODO questa cosa ha un senso ma devo capirlo bene però è praticamente quasi finito
+    //TODO con questa cosa intendo tutto il punto (iii)
+    val someUsers = testData10.map(x => x.user).distinct().take(100)
+    val topArtists = trainData.map(elem => ((elem.user, elem.product), elem.rating))
+      .reduceByKey(_ + _)
+      .sortBy(_._2, ascending = false)
+
+    val top25Artists = someUsers.map(elem => topArtists.filter(obj => obj._1._1.equals(elem))
+      .take(25).map(elem => (elem._1._1, elem._1._2)))
+
+    def actualArtistsForUser(someUser: Int): Set[Int] = {
+      val rawArtistsForUser = rawUserArtistData.map(_.split(' ')).
+        filter { case Array(user,_,_) => user.toInt == someUser }
+      rawArtistsForUser.map(x => x(1).toInt).collect().toSet
+    }
+
+    val artistsTotalCount = trainData.map(r => (r.product, r.rating)).reduceByKey(_ + _).collect().sortBy(-_._2)
+
+    def predictMostPopular(user: Int, numArtists: Int) = {
+      val topArtists = artistsTotalCount.take(numArtists)
+      topArtists.map{case (artist, rating) => Rating(someUser, artist, rating)}
+    }
+
+    var AUC1 = 0.0
+    var AUC2 = 0.0
+
+    for (someUser <- someUsers) {
+
+      val actualArtists = actualArtistsForUser(someUser)
+
+      val recommendations1 = model.recommendProducts(someUser, 25)
+      val predictionsAndLabels1 = recommendations1.map {
+        case Rating(user, artist, rating) =>
+          if (actualArtists.contains(artist)) {
+            (rating, 1.0)
+          } else {
+            (rating, 0.0)
+          }
+      }
+
+      val metrics1 = new BinaryClassificationMetrics(sc.parallelize(predictionsAndLabels1))
+      AUC1 += metrics1.areaUnderROC
+
+      val recommendations2 = predictMostPopular(someUser, 25)
+      val predictionsAndLabels2 = recommendations2.map {
+        case Rating(user, artist, rating) =>
+          if (actualArtists.contains(artist)) {
+            (rating, 1.0)
+          } else {
+            (rating, 0.0)
+          }
+      }
+
+      val metrics2 = new BinaryClassificationMetrics(sc.parallelize(predictionsAndLabels2))
+      AUC2 += metrics2.areaUnderROC
+    }
   }
 }
