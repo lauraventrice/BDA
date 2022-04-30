7b88a36ab13eb8b16419091d89d35eaba27839ed
Gepiro <gepiro0916@gmail.com>
2022-04-30 17:07:35 +0200

    End

--- Exercise2/Problem1/src/main/scala/Problem1.scala
+++ Exercise2/Problem1/src/main/scala/Problem1.scala
@@ -1,18 +1,15 @@
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.ml.classification.{DecisionTreeClassifier, RandomForestClassifier}
 import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.feature.{OneHotEncoder, StandardScaler, StringIndexer, VectorAssembler}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.mllib.linalg._
 import org.apache.spark.mllib.regression._
-import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.tree._
 import org.apache.spark.mllib.tree.model._
 import org.apache.spark.rdd._
 import org.apache.spark.mllib.evaluation._
-import org.apache.spark.mllib.linalg
+import org.apache.spark.sql.Row
 
 object Problem1 {
   def main(args: Array[String]): Unit = {
@@ -60,12 +57,16 @@ object Problem1 {
       .fit(trainData)
 
     //Transform the whole set of String features in OneHotEncoder features
-    val stringIndexer = new StringIndexer()
-      .setInputCols(index_columns)
-      .setOutputCols(index_columns_index)
-    val oneHotEncoder = new OneHotEncoder()
-      .setInputCols(index_columns_index)
-      .setOutputCols(index_columns_OHE)
+    val stringIndexer = index_columns.map { colName =>
+      new StringIndexer()
+        .setInputCol(colName)
+        .setOutputCol(colName + "_index")
+    }
+    val oneHotEncoder = index_columns.map { colName =>
+      new OneHotEncoder()
+        .setInputCol(colName + "_index")
+        .setOutputCol(colName + "OHE")
+    }
 
     //Features' union
     val vector = new VectorAssembler()
@@ -81,47 +82,48 @@ object Problem1 {
       .setFeaturesCol("scaledFeatures")
 
     //Pipeline
-    val stages = Array(labelIndexer, stringIndexer, oneHotEncoder, vector, scaler, cl)
+    val stages = Array(labelIndexer) ++ stringIndexer ++ oneHotEncoder ++ Array(vector, scaler, cl)
     val pipeline = new Pipeline().setStages(stages)
 
     val model = pipeline.fit(trainData)
 
     //See results
     /*model.transform(testData)
-      .select("HeartDisease","probability", "prediction")
+      .select("HeartDisease", "prediction")
       .collect()
-      .foreach { case Row(id: String, prob: Vector, prediction: Double) =>
-        println(s"($id) --> prob=$prob, prediction=$prediction")
+      .foreach { case Row(id: String, prediction: Double) =>
+        println(s"($id) --> prediction=$prediction")
       }*/
 
     //(ii) (iii)
     val paramGrid = new ParamGridBuilder()
-      .addGrid(cl.maxDepth, Array(5)) //TODO add 10,15
+      .addGrid(cl.maxDepth, Array(5, 10, 15))
       .addGrid(cl.impurity, Array("entropy", "gini"))
-      .addGrid(cl.maxBins, Array(20)) //TODO add 50,100
+      .addGrid(cl.maxBins, Array(20, 50, 100))
       .build()
 
     //Cross-validation
     val cv = new CrossValidator()
       .setEstimator(pipeline)
-      .setEvaluator(new BinaryClassificationEvaluator()) //Si può ache mettere semplicemente new BinaryClassificationEvaluator
+      .setEvaluator(new BinaryClassificationEvaluator())
       .setEstimatorParamMaps(paramGrid)
-      .setNumFolds(2)  // Use 5-fold cross-validation //TODO è a 2 per renderlo più veloce ma deve essere 5
+      .setNumFolds(5)  // Use 5-fold cross-validation
       .setParallelism(2)
 
     val cvModel = cv.fit(trainData)
 
     //Model's Average Metrics
-    val avgMetrics = cvModel.avgMetrics //TODO io non credo sia l'accuracy però molto simile a quello che vogliamo penso
+    val avgMetrics = cvModel.avgMetrics
 
     cvModel.save("./tree") //Saving Model
 
     //See results
-    /*cvModel.transform(testData)
-      .select("HeartDisease","probability", "prediction")
+    /*
+    cvModel.transform(testData)
+      .select("HeartDisease", "prediction")
       .collect()
-      .foreach { case Row(id: String, prob: Vector, prediction: Double) =>
-        println(s"($id) --> prob=$prob, prediction=$prediction")
+      .foreach { case Row(id: String, prediction: Double) =>
+        println(s"($id) --> prediction=$prediction")
       }*/
 
     //(iv)
@@ -153,51 +155,50 @@ object Problem1 {
       .setFeaturesCol("scaledFeatures")
       .setFeatureSubsetStrategy("auto")
 
-    val stagesForest = Array(labelIndexer, stringIndexer, oneHotEncoder, vector, scaler, rf)
+
+    val stagesForest = Array(labelIndexer) ++ stringIndexer ++ oneHotEncoder ++ Array(vector, scaler, rf)
     val pipelineForest = new Pipeline().setStages(stagesForest)
 
     val paramForest = new ParamGridBuilder()
       .addGrid(rf.impurity, Array("entropy", "gini"))
-      .addGrid(rf.numTrees, Array(5)) //TODO add 10, 20
-      .addGrid(rf.maxDepth, Array(5)) //TODO add 10,15
-      .addGrid(rf.maxBins, Array(20)) //TODO add 50,100
+      .addGrid(rf.numTrees, Array(5, 10, 20))
+      .addGrid(rf.maxDepth, Array(5, 10, 15))
+      .addGrid(rf.maxBins, Array(20, 50, 100))
       .build()
 
     val cvForest = new CrossValidator()
       .setEstimator(pipelineForest)
       .setEvaluator(new BinaryClassificationEvaluator())
       .setEstimatorParamMaps(paramForest)
-      .setNumFolds(2)  // Use 5-fold cross-validation //TODO è a 2 per renderlo più veloce ma deve essere 5
+      .setNumFolds(5)  // Use 5-fold cross-validation
       .setParallelism(2)
 
     val cvModelForest = cvForest.fit(trainData)
 
     //See results
     /*cvModelForest.transform(testData)
-      .select("HeartDisease","probability", "prediction")
+      .select("HeartDisease", "prediction")
       .collect()
-      .foreach { case Row(id: String, prob: Vector, prediction: Double) =>
-        println(s"($id) --> prob=$prob, prediction=$prediction")
+      .foreach { case Row(id: String, prediction: Double) =>
+        println(s"($id) --> prediction=$prediction")
       }*/
 
     val predictionAndLabelsForest = cvModelForest.transform(testData).select("label", "prediction").rdd.map(row => (row.getDouble(0), row.getDouble(1)))
-    val metricsForest = new BinaryClassificationMetrics(predictionAndLabelsForest)
 
     //Model's accuracy
-    val accuracyForest = predictionAndLabels.filter(pl => pl._1.equals(pl._2)).count().toDouble / testData.count().toDouble
+    val accuracyForest = predictionAndLabelsForest.filter(pl => pl._1.equals(pl._2)).count().toDouble / testData.count().toDouble
     println("Accuracy: " + accuracyForest)
 
-    //Professor's Models
-    dataFrame = labelIndexer.transform(dataFrame)
-    dataFrame = stringIndexer.fit(dataFrame).transform(dataFrame)
-    dataFrame = oneHotEncoder.fit(dataFrame).transform(dataFrame)
-    dataFrame = vector.transform(dataFrame)
-    dataFrame = scaler.fit(dataFrame).transform(dataFrame)
 
-    val data2 = dataFrame.rdd.map(row => LabeledPoint(
+    //Professor's Models
+    val pipeStages = Array(labelIndexer) ++ stringIndexer ++ oneHotEncoder ++ Array(vector)
+    val indexer_model = new Pipeline().setStages(pipeStages).fit(dataFrame)
+
+    val df = indexer_model.transform(dataFrame)
+
+    val data2 = df.rdd.map(row => LabeledPoint(
       row.getAs[Double]("label"),
-      org.apache.spark.mllib.linalg.Vectors.fromML(row.getAs("features"))
-    )
+      org.apache.spark.mllib.linalg.Vectors.fromML(row.getAs("features")))
     )
 
     def getMetrics(model: DecisionTreeModel, data: RDD[LabeledPoint]):
@@ -217,7 +218,7 @@ object Problem1 {
            bins <- Array(200, 300))
       yield {
         val model2 = DecisionTree.trainClassifier(
-          trainData2, 2, Map(3 -> 2, 4 -> 2), //TODO non so cosa scrivere in questa Map
+          trainData2, 2, Map(0 -> 2, 1 -> 2, 2 -> 2, 3 -> 2, 4 -> 2, 5 -> 6, 6 -> 4, 7 -> 2, 8 -> 5, 9 -> 2, 10 -> 2, 11 -> 2, 12 -> 13),
           impurity, depth, bins)
         val trainAccuracy = getMetrics(model2, trainData2).accuracy
         val valAccuracy = getMetrics(model2, valData2).accuracy
@@ -225,8 +226,9 @@ object Problem1 {
 
     evaluations2.sortBy(_._2).reverse.foreach(println)
 
+    //Compute best tree's accuracy
     val model2 = DecisionTree.trainClassifier(
-      trainData2, 2, Map(3 -> 2, 4 -> 2), //TODO stesso di sopra
+      trainData2, 2, Map(0 -> 2, 1 -> 2, 2 -> 2, 3 -> 2, 4 -> 2, 5 -> 6, 6 -> 4, 7 -> 2, 8 -> 5, 9 -> 2, 10 -> 2, 11 -> 2, 12 -> 13),
       "gini", 30, 300)
 
     val testAccuracy = getMetrics(model2, testData2).accuracy
