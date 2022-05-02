9b4d901f8fe27693aee52eb277bfda314d54b08e
Gepiro <gepiro0916@gmail.com>
2022-05-02 23:44:26 +0200

    Some funny changes

--- Exercise2/Problem3/src/main/scala/Problem3.scala
+++ Exercise2/Problem3/src/main/scala/Problem3.scala
@@ -1,7 +1,5 @@
-import breeze.signal.OptWindowFunction.User
 import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd._
 import org.apache.spark.mllib.recommendation._
 
 object Problem3 {
@@ -9,9 +7,8 @@ object Problem3 {
     val sparkConf = new SparkConf().setAppName("Problem3")
     val sc = new SparkContext(sparkConf)
 
-    val sampleSize = 0.01 // again use 1 percent sample size for debugging!
     val rawArtistAlias = sc.textFile("./Data/audioscrobbler/artist_alias.txt")
-    val rawUserArtistData = sc.textFile("./Data/audioscrobbler/user_artist_data.txt").sample(false, sampleSize)
+    val rawUserArtistData = sc.textFile("./Data/audioscrobbler/user_artist_data.txt")
 
     val artistAlias = rawArtistAlias.flatMap { line =>
       val tokens = line.split('\t')
@@ -35,30 +32,36 @@ object Problem3 {
 
     //(a)
     //(i)
+    //users who listened to at least 100 distinct artists (72,748)
     val counts = trainData.map(elem => (elem.user, 1))
       .reduceByKey(_ + _)
-      .filter(elem => elem._2 >= 50)//TODO metti 100, per il momento lascio 50 per i test poichè ho il dataset rimpicciolito
+      .filter(elem => elem._2 >= 100)
       .map(c => c._1)
       .collect()
 
-    val trainData100 = trainData.filter(elem => counts contains elem.user)
-    //println(trainData100.count())
+    //Rating objects of only the users above
+    val trainData100 = trainData.filter(elem => counts.contains(elem.user))
 
     //(ii)
-    val Array(trainData90, testData10) = trainData100.randomSplit(Array(0.9, 0.1)) //TODO sono sicuro che la divisione sia come la vuole il prof (?)
+    //Split the rdd in two different rdd in which for each user in trainData100,
+    // there are about 90% of the artists this user has listened in trainData90
+    // while the remaining 10% are in testData10
+    val Array(trainData90, testData10) = trainData100.map(x => (x.user, x))
+      .groupByKey()
+      .flatMap(x => x._2)
+      .randomSplit(Array(0.9, 0.1))
 
-    //TODO va bene questo metodo per training ? Il prof usa questo nel suo file
+    //Train the model
     val model = ALS.trainImplicit(trainData90, 10, 5, 0.01, 1.0)
 
     //(iii)
-
-    //TODO questa cosa ha un senso ma devo capirlo bene però è praticamente quasi finito
-    //TODO con questa cosa intendo tutto il punto (iii)
+    //Take 100 random users
     val someUsers = testData10.map(x => x.user).distinct().take(100)
-    val topArtists = trainData.map(elem => ((elem.user, elem.product), elem.rating))
+    val topArtists = trainData100.map(elem => ((elem.user, elem.product), elem.rating))
       .reduceByKey(_ + _)
       .sortBy(_._2, ascending = false)
 
+    //Top 25 artist for the 100 random users
     val top25Artists = someUsers.map(elem => topArtists.filter(obj => obj._1._1.equals(elem))
       .take(25).map(elem => (elem._1._1, elem._1._2)))
 
@@ -68,14 +71,13 @@ object Problem3 {
       rawArtistsForUser.map(x => x(1).toInt).collect().toSet
     }
 
-    val artistsTotalCount = trainData.map(r => (r.product, r.rating)).reduceByKey(_ + _).collect().sortBy(-_._2)
+    val artistsTotalCount = trainData100.map(r => (r.product, r.rating)).reduceByKey(_ + _).collect().sortBy(-_._2)
 
     def predictMostPopular(user: Int, numArtists: Int) = {
       val topArtists = artistsTotalCount.take(numArtists)
       topArtists.map{case (artist, rating) => Rating(user, artist, rating)}
     }
 
-    //TODO ho scritto questo metodo per richiamarlo poi dopo
     def getAUC(someUser: Int, actualArtists: Set[Int], model: MatrixFactorizationModel) = {
         val recommendations = model.recommendProducts(someUser, 25)
         val predictionsAndLabels = recommendations.map {
@@ -100,6 +102,7 @@ object Problem3 {
       AUC
     }
 
+    /*
     var AUC1 = 0.0
     var AUC2 = 0.0
 
@@ -119,22 +122,48 @@ object Problem3 {
           }
       }
 
-      val metrics2 = new BinaryClassificationMetrics(sc.parallelize(predictionsAndLabels2)),
+      val metrics2 = new BinaryClassificationMetrics(sc.parallelize(predictionsAndLabels2))
       AUC2 += metrics2.areaUnderROC
     }
 
+     */
     //(b)
 
-    val evaluations = for(rank <- Array(10, 25); //TODO add 50
-                          lambda <- Array(1.0, 0.01); //TODO add 0.001
-                          alpha <- Array(1.0)) //TODO add 10.0, 100.0
+    val evaluations = for(rank <- Array(10, 25, 50);
+                          lambda <- Array(1.0, 0.01, 0.001);
+                          alpha <- Array(1.0, 10.0, 100.0))
     yield {
-      val modelCrossValidation = ALS.trainImplicit(trainData, rank, 5, lambda, alpha)//TODO credo che quel 5 iterazioni intenda il k del cross validation, di conseguenza potrebbeero essere le fold?
+      val modelCrossValidation = ALS.trainImplicit(trainData90, rank, 5, lambda, alpha)
       val auc = getTotalAUC(modelCrossValidation)
       ((rank, lambda, alpha), auc)
     }
     evaluations.sortBy(_._2).reverse.foreach(println)
 
+    /*
+    //TODO metti i parametri del modello migliore
+    val bestModel = ALS.trainImplicit(trainData90, 10, 5, 10, 10)
+
+    val predictionsAndLabels: Array[(Double, Double)] = null
+    for(someUser <- someUsers) {
+      val actualArtists = actualArtistsForUser(someUser)
+      val recommendations = model.recommendProducts(someUser, 25)
+      predictionsAndLabels +: recommendations.map {
+        case Rating(user, artist, rating) =>
+          if (actualArtists.contains(artist)) {
+            (rating, 1.0)
+          } else {
+            (rating, 0.0)
+          }
+      }
+    }
+    val metrics = new BinaryClassificationMetrics(sc.parallelize(predictionsAndLabels))
+    metrics.areaUnderROC()
+    metrics.precisionByThreshold()
+    metrics.recallByThreshold()
+    //TODO calcola accuracy
+
+    //TODO calcola il runtime
+     */
     //(c)
     //New user information
     /*val newUserList = List(Rating(1609994, 7007868, 113), Rating(1609994, 10191561, 53), Rating(1609994, 10308181, 23),
@@ -144,7 +173,7 @@ object Problem3 {
     val newUser = sc.parallelize(newUserList)
 
     //New trainData
-    val trainDataModified = trainData.union(newUser)
+    val trainDataModified = trainData90.union(newUser)
     //Model with best choice
     //TODO trova le best choice
     // val model = ALS.trainImplicit(trainData90, 10, 5, 0.01, 1.0)*/
