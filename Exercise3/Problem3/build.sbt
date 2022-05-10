8c137f03f1f7f7a21968af2453ce3a897ab62875
Gepiro <gepiro0916@gmail.com>
2022-05-10 13:08:39 +0200

    Init

---
+++ Exercise3/Problem3/build.sbt
@@ -0,0 +1,10 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.12.15"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "Problem3"
+  )
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.1"
---
+++ Exercise3/Problem3/src/main/scala/Problem3.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Problem3 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName(this.getClass.getSimpleName)
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+
+    
+  }
+}
