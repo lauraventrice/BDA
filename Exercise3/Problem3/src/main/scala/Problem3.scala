4fca85b0bcb476e802f649c2db9aee0d8362d1b7
Gepiro <gepiro0916@gmail.com>
2022-05-12 14:24:27 +0200

    Progress (a)

--- Exercise3/Problem3/src/main/scala/Problem3.scala
+++ Exercise3/Problem3/src/main/scala/Problem3.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.graphx._
 
 object Problem3 {
   def main(args: Array[String]): Unit = {
@@ -6,6 +7,23 @@ object Problem3 {
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
 
-    
+    //(a)
+    val file = sc.textFile("./twitter_combined.txt")
+
+    val edges = file.map(elem => {
+      val edge = elem.split(" ")
+      Edge(edge(0).toLong, edge(1).toLong, 1) // TODO perchè ho messo 1? Boh sandro chi lo sa!
+    })
+
+    val vertices = file.flatMap(elem => {
+      elem.split(" ")
+    }).distinct().map(p => (p.toLong, p.toLong)) // TODO parse the features from the additional files and assign them as the vertex labels
+    // TODO prossima volta scrivilo ancora dopo prof
+
+    val graph = Graph(vertices, edges).cache()
+
+    //(b)
+    val connectedComponentGraph: Graph[VertexId, Int] = graph.connectedComponents()
+
   }
 }
