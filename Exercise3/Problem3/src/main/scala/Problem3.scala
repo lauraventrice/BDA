745480f90207dfe1edc06c0f16819354042957eb
Gepiro <gepiro0916@gmail.com>
2022-05-17 15:57:03 +0200

    Boh non so nemmeno più che sto a fa'

--- Exercise3/Problem3/src/main/scala/Problem3.scala
+++ Exercise3/Problem3/src/main/scala/Problem3.scala
@@ -1,5 +1,10 @@
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx._
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.stat.ChiSquareTest
+import org.apache.spark.sql.SparkSession
+
 
 object Problem3 {
   def main(args: Array[String]): Unit = {
@@ -7,12 +12,17 @@ object Problem3 {
     val sc = new SparkContext(conf)
     sc.setLogLevel("ERROR")
 
+    val spark:SparkSession = SparkSession.builder()
+      .master("local[1]").appName("Session")
+      .getOrCreate()
+
     //(a)
     val file = sc.textFile("./twitter_combined.txt")
     val features = sc.wholeTextFiles("./twitter/*.feat")
     val featuresName = sc.wholeTextFiles("./twitter/*.featnames")
     val featuresEGO = sc.wholeTextFiles("./twitter/*.egofeat")
 
+    // Parse the feature for each node
     var results = features.map(elem => (elem._1+"names", elem._2))
       .join(featuresName)
       .map { case (fileName, (featBin, featName)) => (featBin, featName)}
@@ -46,6 +56,7 @@ object Problem3 {
       (tmp, elem.drop(tmp.length+1))
     }).groupByKey().map(elem => (elem._1, elem._2.toArray))
 
+    // Edges
     val arrayEdges = file.countByValue().toArray.map(elem => {
       val edge = elem._1.split(" ")
       Edge(edge(0).toLong, edge(1).toLong, elem._2.toInt)
@@ -53,12 +64,13 @@ object Problem3 {
 
     val edges = sc.parallelize(arrayEdges)
 
+    // Vertices
     val vertices = featureNames.map(elem => (elem._1.toLong, elem._2))
 
     val graph = Graph(vertices, edges).cache()
 
+    /*
     //(b)
-    // TODO è sbagliato il risultato
     val connectedComponentGraph: Graph[VertexId, Int] = graph.connectedComponents()
 
     def sortedConnectedComponents(connectedComponents: Graph[VertexId, _]): Seq[(VertexId, Long)] = {
@@ -69,21 +81,25 @@ object Problem3 {
     val componentCounts = sortedConnectedComponents(connectedComponentGraph)
     println(componentCounts.size)
 
-    /*
+     */
+
     //(c)
     // Degree-distribution
     val degrees: VertexRDD[Int] = graph.degrees.cache()
     println(degrees.map(_._2).stats())
 
-    def topNamesAndDegrees(degrees: VertexRDD[Int], topicGraph: Graph[String, Int]): Array[(String, Int)] = {
+    /*
+    def topNamesAndDegrees(degrees: VertexRDD[Int], topicGraph: Graph[Array[String], Int]): Array[(String, Int)] = {
       val namesAndDegrees = degrees.innerJoin(topicGraph.vertices) {
-        (vertexId, degree, name) => (name, degree) }
+        (vertexId, degree, name) => (name.mkString("Array(", ", ", ")"), degree) }
       val ord = Ordering.by[(String, Int), Int](_._2)
       namesAndDegrees.map(_._2).top(10)(ord)
     }
 
     topNamesAndDegrees(degrees, graph).foreach(println)
 
+     */
+    /*
     // Average clustering-coefficient
     val triangleCountGraph = graph.triangleCount()
     println(triangleCountGraph.vertices.map(x => x._2).stats())
@@ -94,6 +110,10 @@ object Problem3 {
       }
     println(clusterCoefficientGraph.map(_._2).sum() / graph.vertices.count())
 
+
+     */
+
+    /*
     // Average Path Length
     def mergeMaps(m1: Map[VertexId, Int], m2: Map[VertexId, Int]): Map[VertexId, Int] = {
       def minThatExists(k: VertexId): Int = {
@@ -119,8 +139,6 @@ object Problem3 {
       checkIncrement(e.srcAttr, e.dstAttr, e.dstId) ++ checkIncrement(e.dstAttr, e.srcAttr, e.srcId)
     }
 
-    // TODO togli il sample
-    // TODO prova su cluster perchè da java heap error :(
     val sampleVertices = graph.vertices.map(v => v._1).sample(false, 0.01).collect().toSet
 
     val mapGraph = graph.mapVertices((id, _) => {
@@ -130,10 +148,12 @@ object Problem3 {
         Map[VertexId, Int]()
       }
     })
+    println("A")
 
     // Start the Pregel-style form of iterative breadth-first search
     val start = Map[VertexId, Int]()
     val res = mapGraph.pregel(start)(update, iterate, mergeMaps)
+    println("B")
 
     val paths = res.vertices.flatMap {
       case (id, m) =>
@@ -148,6 +168,7 @@ object Problem3 {
     val hist = paths.map(_._3).countByValue()
     hist.toSeq.sorted.foreach(println)
 
+
      */
 
     /*
@@ -157,28 +178,52 @@ object Problem3 {
     val namesAndRanks = ranks.innerJoin(graph.vertices) {
       (vertexId, rank, name) => (name, rank)
     }
-    val ord = Ordering.by[(String, Double), Double](_._2)
-    val top250 = namesAndRanks.map(_._2).top(250)(ord)
-    top250.foreach(println)
-
-    println("COMPARE")
+    val ord = Ordering.by[(Array[String], Double), Double](_._2)
+    val top250 = namesAndRanks.map(_._2).top(250)(ord).map(elem => (elem._1.mkString("Array(", ", ", ")"), elem._2))
 
     val degreesInOut: VertexRDD[Int] = graph.degrees.cache()
     val namesAndRanksToCompare = degreesInOut.innerJoin(graph.vertices) {
-      (topicId, rank, name) => (name, rank)
+      (vertexId, rank, name) => (name, rank)
     }
-    val ordToCompare = Ordering.by[(String, Int), Int](_._2)
-    val top250ToCompare = namesAndRanksToCompare.map(_._2).top(250)(ordToCompare)
-    top250ToCompare.foreach(println)
+    val ordToCompare = Ordering.by[(Array[String], Int), Int](_._2)
+    val top250ToCompare = namesAndRanksToCompare.map(_._2).top(250)(ordToCompare).map(elem => (elem._1.mkString("Array(", ", ", ")"), elem._2))
 
     val equalVertex = top250.filter(elem => top250ToCompare.map(_._1).contains(elem._1))
-    println("AAAAAAAAAA")
+    println("Equal")
     println(equalVertex.length)
-    equalVertex.foreach(println)
 
      */
 
     //(e)
-    
+    def inRange(int: Int, string: String) = {
+      val tmp = string.replace("[", "").replace(")","").split(",")
+      var boolean = false
+      if(string.equals("[90,inf)")){
+        boolean = int >= tmp(0).toInt
+      }
+      else {
+        boolean = int >= tmp(0).toInt && int < tmp(1).toInt
+      }
+      boolean
+    }
+
+    val bucket = List("[0,10)", "[10,20)", "[20,30)", "[30,40)", "[40,50)", "[50,60)", "[60,70)", "[70,80)", "[80,90)", "[90,inf)")
+    val degreesMap = degrees.map(elem => (elem._1, elem._2)).flatMap(x => bucket.collect{
+      case i if inRange(x._2, i) => (x._1.toLong, i)
+    })
+
+    val featuresMap = featureNames.map(elem => (elem._1, elem._2.length)).flatMap(x => bucket.collect{
+      case i if inRange(x._2, i) => (x._1.toLong, i)
+    })
+
+    val prova = degreesMap.join(featuresMap).groupBy(elem => elem._2).map(elem => (elem._1.toString(), Vectors.dense(elem._2.size)))
+
+    val a = spark.createDataFrame(prova).toDF()
+
+    val b = new StringIndexer().setInputCol("_1").setOutputCol("label").fit(a).transform(a)
+
+    val chi = ChiSquareTest.test(b, "_2", "label").head
+    println(s"pValues = ${chi.getAs[Vector](0)}")
+
   }
 }
