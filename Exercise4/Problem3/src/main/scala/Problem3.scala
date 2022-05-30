dcac355730ecc04f23c9c4c29b8985c73f133f6f
Laura <laura.ventrice@edu.unito.it>
2022-05-30 19:53:59 +0200

    continue (f)

--- Exercise4/Problem3/src/main/scala/Problem3.scala
+++ Exercise4/Problem3/src/main/scala/Problem3.scala
@@ -15,8 +15,9 @@ import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, StringType, StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.ml.classification.{DecisionTreeClassifier, RandomForestClassifier}
-
 import org.apache.spark.graphx._
+import org.apache.spark.ml.stat.ChiSquareTest
+
 import java.io.{BufferedWriter, File, FileWriter}
 import scala.collection.mutable.ArrayBuffer
 
@@ -317,11 +318,115 @@ object Problem3 {
 
     val edgesCount = edges.count()
     val verticesCount = verticesPokemon.count()
-    print(s"VERTICES: $verticesCount")
-    print(s"EDGES: $edgesCount")
+    println(s"VERTICES: $verticesCount")
+    println(s"EDGES: $edgesCount")
 
     val pokemonGraph = Graph(verticesPokemon, edges)
-    
-    
+
+    //connected component
+
+    val connectedComponentGraph: Graph[VertexId, Int] = pokemonGraph.connectedComponents()
+
+    def sortedConnectedComponents(connectedComponents: Graph[VertexId, _]): Seq[(VertexId, Long)] = {
+      val componentCounts = connectedComponents.vertices.map(_._2).countByValue
+      componentCounts.toSeq.sortBy(_._2).reverse
+    }
+
+    val componentCounts = sortedConnectedComponents(connectedComponentGraph)
+    println(componentCounts.size)
+
+    //Degree-distribution
+    val degrees: VertexRDD[Int] = pokemonGraph.degrees.cache()
+    println(degrees.map(_._2).stats())
+
+
+    def topNamesAndDegrees(degrees: VertexRDD[Int], topicGraph: Graph[Array[String], Int]): Array[(String, Int)] = {
+      val namesAndDegrees = degrees.innerJoin(topicGraph.vertices) {
+        (vertexId, degree, name) => (name.mkString("Array(", ", ", ")"), degree) }
+      val ord = Ordering.by[(String, Int), Int](_._2)
+      namesAndDegrees.map(_._2).top(10)(ord)
+    }
+
+    topNamesAndDegrees(degrees, graph).foreach(println)
+
+    // Average clustering-coefficient
+    val triangleCountGraph = pokemonGraph.triangleCount()
+    println(triangleCountGraph.vertices.map(x => x._2).stats())
+
+    val maxTriangleGraph = pokemonGraph.degrees.mapValues(d => d * (d - 1) / 2.0)
+    val clusterCoefficientGraph = triangleCountGraph.vertices.innerJoin(maxTriangleGraph) {
+      (vertexId, triCount, maxTris) => {if (maxTris == 0) 0 else triCount / maxTris }
+    }
+    println(clusterCoefficientGraph.map(_._2).sum() / pokemonGraph.vertices.count())
+
+
+    // PageRank API
+    val ranks = pokemonGraph.pageRank(0.001, 0.15).vertices
+    val namesAndRanks = ranks.innerJoin(pokemonGraph.vertices) {
+      (vertexId, rank, name) => (name, rank)
+    }
+    val ord = Ordering.by[(Array[String], Double), Double](_._2)
+    val top250 = namesAndRanks.map(_._2).top(250)(ord).map(elem => (elem._1.mkString("Array(", ", ", ")"), elem._2))
+
+    val namesAndRanksToCompare = degrees.innerJoin(pokemonGraph.vertices) {
+      (vertexId, rank, name) => (name, rank)
+    }
+    val ordToCompare = Ordering.by[(Array[String], Int), Int](_._2)
+    val top250ToCompare = namesAndRanksToCompare.map(_._2).top(250)(ordToCompare).map(elem => (elem._1.mkString("Array(", ", ", ")"), elem._2))
+
+    val equalVertex = top250.filter(elem => top250ToCompare.map(_._1).contains(elem._1))
+    println("Equal")
+    println(equalVertex.length)
+
+    //chi square
+    def inRange(int: Int, string: String) = {
+      val tmp = string.replace("[", "").replace(")","").split(",")
+      var boolean = false
+      if(string.equals("[90,inf)")){
+        boolean = int >= tmp(0).toInt
+      }
+      else {
+        boolean = int >= tmp(0).toInt && int < tmp(1).toInt
+      }
+      boolean
+    }
+
+    val bucket = Array("[0,10)", "[10,20)", "[20,30)", "[30,40)", "[40,50)", "[50,60)", "[60,70)", "[70,80)", "[80,90)", "[90,inf)")
+
+    def addZeroElement(rdd: RDD[(String, Vector)]) = {
+      val tmp: ArrayBuffer[String] = ArrayBuffer.empty
+      bucket.indices.foreach(int => {
+        bucket.indices.foreach(index => {
+          tmp.append(bucket(int) + "," + bucket(index))
+        })
+      })
+      val rddDiff: ArrayBuffer[(String, Vector)] = ArrayBuffer.empty
+      tmp.foreach(elem => {
+        if(rdd.filter(x => x._1.equals(elem)).count() == 0.0){
+          rddDiff.append((elem, Vectors.dense(0.0)))
+        }
+      })
+      rddDiff
+    }
+
+    val degreesMap = degrees.map(elem => (elem._1, elem._2)).flatMap(x => bucket.collect{
+      case i if inRange(x._2, i) => (x._1.toLong, i)
+    })
+
+    val featuresMap = featureNames.map(elem => (elem._1, elem._2.length)).flatMap(x => bucket.collect{
+      case i if inRange(x._2, i) => (x._1.toLong, i)
+    })
+
+    var rdd = degreesMap.join(featuresMap).groupBy(elem => elem._2).map(elem => (elem._1.toString(), Vectors.dense(elem._2.size)))
+
+    rdd = rdd.union(sc.parallelize(addZeroElement(rdd)))
+
+    var dataframe = spark.createDataFrame(rdd).toDF()
+
+    dataframe = new StringIndexer().setInputCol("_1").setOutputCol("label").fit(dataframe).transform(dataframe)
+
+    val chi = ChiSquareTest.test(dataframe, "_2", "label").head
+    println(s"pValues = ${chi.getAs[Vector](0)}")
+
   }
 }
